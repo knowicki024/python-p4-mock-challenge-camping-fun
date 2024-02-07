361967fe2ceda5be6a031349b882f026d671dc71
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-02-07 12:25:11 -0500

    B&B

--- server/app.py
+++ server/app.py
@@ -25,5 +25,131 @@ db.init_app(app)
 def home():
     return ''
 
+@app.route('/campers', methods=['GET', 'POST'])
+def campers():
+    try:
+        if request.method == 'GET':
+            campers = Camper.query.all()
+
+            campers_dict = [camper.to_dict(rules = ('-signups',)) for camper in campers] 
+            
+            response = make_response(
+                jsonify(campers_dict),
+                200
+            )
+
+        elif request.method == 'POST':
+            form_data = request.get_json()
+            new_camper = Camper(
+                name = form_data['name'],
+                age = form_data['age']
+            )
+            db.session.add(new_camper)
+            db.session.commit()
+            response = make_response(
+                new_camper.to_dict(),
+                201
+            )
+    except:
+        response = make_response(
+            { "errors": ["validation errors"] },
+            400
+        )
+
+    return response
+    
+
+@app.route('/campers/<int:id>', methods=['GET', 'PATCH'])
+def campers_by_id(id):
+    camper = Camper.query.filter(Camper.id == id).first()
+    
+    if camper:
+        if request.method == 'GET':
+            response = make_response(
+                camper.to_dict(),
+                200
+            )
+        elif request.method == 'PATCH':
+            try:
+                form_data = request.get_json()
+                for attr in form_data:
+                    setattr(camper, attr, form_data[attr])
+                db.session.commit()
+                response = make_response(
+                    camper.to_dict(),
+                    202
+                )
+            except ValueError:
+                response = make_response(
+                    {"errors": ["validation errors"]},
+                    400
+                )
+
+
+    else:
+        response = make_response(
+            {"error": "Camper not found"},
+            404
+        )
+    return response 
+
+@app.route('/activities', methods=['GET'])
+def activities():
+    activities = Activity.query.all()
+
+    activity_dict = [activity.to_dict(rules=('-signups',)) for activity in activities]
+    
+    response = make_response(
+        jsonify(activity_dict),
+        200
+    )
+    return response
+
+@app.route('/activities/<int:id>', methods=['DELETE'])
+def activites_by_id(id):
+    activity = Activity.query.filter(Activity.id ==id).first()
+
+    if activity:
+        db.session.delete(activity)
+        db.session.commit()
+        response = make_response(
+            {},
+            204
+        )
+    else:
+        response = make_response(
+            {"error": "Activity not found"},
+            404
+        )
+    return response
+
+@app.route('/signups', methods=['POST'])
+def signups():
+    try:
+        form_data = request.get_json()
+        new_signup = Signup(
+            camper_id = form_data['camper_id'],
+            activity_id = form_data['activity_id'],
+            time = form_data['time']
+        )
+        db.session.add(new_signup)
+        db.session.commit()
+        response = make_response(
+            new_signup.to_dict(),
+            201
+        )
+    except:
+        response = make_response(
+            { "errors": ["validation errors"] },
+            400
+        )
+
+    return response
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -25,8 +25,10 @@ class Activity(db.Model, SerializerMixin):
     difficulty = db.Column(db.Integer)
 
     # Add relationship
+    signups = db.relationship('Signup', back_populates= 'activity', cascade='all, delete-orphan')
     
     # Add serialization rules
+    serialize_rules = ('-signups.activity',)
     
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -40,11 +42,23 @@ class Camper(db.Model, SerializerMixin):
     age = db.Column(db.Integer)
 
     # Add relationship
+    signups = db.relationship('Signup', back_populates='camper', cascade='all, delete-orphan')
     
     # Add serialization rules
+    serialize_rules = ('-signups.camper',)
     
     # Add validation
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError('Invalid name')
+        return value 
     
+    @validates('age')
+    def validate_age(self, key, value):
+        if value is None or not (8 <= value <= 18):
+            raise ValueError('Invalid age')
+        return value 
     
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
@@ -57,10 +71,21 @@ class Signup(db.Model, SerializerMixin):
     time = db.Column(db.Integer)
 
     # Add relationships
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+
+    activity = db.relationship('Activity', back_populates='signups')
+    camper = db.relationship('Camper', back_populates='signups')
     
     # Add serialization rules
+    serialize_rules = ('-camper.signups', '-activity.signups')
     
     # Add validation
+    @validates('time')
+    def validate_time(self, key, value):
+        if value is None or not (0 <= value <= 23):
+            raise ValueError('Invalid time')
+        return value
     
     def __repr__(self):
         return f'<Signup {self.id}>'
